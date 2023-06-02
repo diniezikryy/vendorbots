903fccb16264ea40eb7dd0484f38e46ed4ac2243
dinie zikry <dzrudi23@gmail.com>
2023-06-03 03:23:23 +0800

    filled up order details with data

--- vendorbots/src/pages/dashboard/orders/[id].jsx
+++ vendorbots/src/pages/dashboard/orders/[id].jsx
@@ -8,6 +8,8 @@ import {
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const invoice = {
   subTotal: "$8,800.00",
@@ -49,7 +51,7 @@ const invoice = {
   ],
 };
 
-export default function OrderDetailsPage() {
+export default function OrderDetailsPage(props) {
   const router = useRouter();
   const { id } = router.query;
 
@@ -67,231 +69,214 @@ export default function OrderDetailsPage() {
 
   const { isLoading, data } = useQuery("order details", getOrderDetails);
 
+  console.log(data.order_items);
+
   return (
     <main>
-      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {/* Invoice summary */}
-          <div className="lg:col-start-3 lg:row-end-1">
-            <h2 className="sr-only">Summary</h2>
-            <div className="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
-              <dl className="flex flex-wrap">
-                <div className="flex-auto pl-6 pt-6">
-                  <dt className="text-sm font-semibold leading-6 text-gray-900">
-                    Amount
-                  </dt>
-                  <dd className="mt-1 text-base font-semibold leading-6 text-gray-900">
-                    $10,560.00
-                  </dd>
-                </div>
-                <div className="flex-none self-end px-6 pt-4">
-                  <dt className="sr-only">Status</dt>
-                  <dd className="rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-600 ring-1 ring-inset ring-green-600/20">
-                    Paid
-                  </dd>
-                </div>
-                <div className="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6">
-                  <dt className="flex-none">
-                    <span className="sr-only">Client</span>
-                    <UserCircleIcon
-                      className="h-6 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </dt>
-                  <dd className="text-sm font-medium leading-6 text-gray-900">
-                    Alex Curren
-                  </dd>
-                </div>
-                <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
-                  <dt className="flex-none">
-                    <span className="sr-only">Due date</span>
-                    <CalendarDaysIcon
-                      className="h-6 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </dt>
-                  <dd className="text-sm leading-6 text-gray-500">
-                    <time dateTime="2023-01-31">January 31, 2023</time>
-                  </dd>
-                </div>
-                <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
-                  <dt className="flex-none">
-                    <span className="sr-only">Status</span>
-                    <CreditCardIcon
-                      className="h-6 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </dt>
-                  <dd className="text-sm leading-6 text-gray-500">
-                    Paid with MasterCard
+      {isLoading ? (
+        <div>
+          <h1>{props.title || <Skeleton />}</h1>
+          {props.body || <Skeleton count={10} />}
+        </div>
+      ) : (
+        <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {/* Invoice summary */}
+            <div className="lg:col-start-3 lg:row-end-1">
+              <h2 className="sr-only">Summary</h2>
+              <div className="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
+                <dl className="flex flex-wrap">
+                  <div className="flex-auto pl-6 pt-6">
+                    <dt className="text-sm font-semibold leading-6 text-gray-900">
+                      Total
+                    </dt>
+                    <dd className="mt-1 text-base font-semibold leading-6 text-gray-900">
+                      {data.total_cost}
+                    </dd>
+                  </div>
+                  <div className="flex-none self-end px-6 pt-4">
+                    <dt className="sr-only">Status</dt>
+                    <dd className="rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-600 ring-1 ring-inset ring-green-600/20">
+                      {data.paid ? <span>Paid</span> : <span>Unpaid</span>}
+                    </dd>
+                  </div>
+                  <div className="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6">
+                    <dt className="flex-none">
+                      <span className="sr-only">Client</span>
+                      <UserCircleIcon
+                        className="h-6 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd className="text-sm font-medium leading-6 text-gray-900">
+                      {data.customer.customer_username}
+                    </dd>
+                  </div>
+                  <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
+                    <dt className="flex-none">
+                      <span className="sr-only">Due date</span>
+                      <CalendarDaysIcon
+                        className="h-6 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd className="text-sm leading-6 text-gray-500">
+                      <time>{data.date.toDate().toLocaleString()}</time>
+                    </dd>
+                  </div>
+                  <div className="my-4 flex w-full flex-none gap-x-4 px-6">
+                    <dt className="flex-none">
+                      <span className="sr-only">Status</span>
+                      <CreditCardIcon
+                        className="h-6 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd className="text-sm leading-6 text-gray-500">
+                      Paid with Paylah!
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+
+            {/* Invoice */}
+            <div className="-mx-4 px-4 py-8 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-16">
+              <h2 className="text-base font-semibold leading-6 text-gray-900">
+                Invoice ID: {id}
+              </h2>
+              <dl className="mt-6 grid grid-cols-1 text-sm leading-6 sm:grid-cols-2">
+                <div className="sm:pr-4">
+                  <dt className="inline text-gray-500">Issued on</dt>{" "}
+                  <dd className="inline text-gray-700">
+                    <time className="text-gray-500">
+                      {data.date.toDate().toDateString()}
+                    </time>
                   </dd>
                 </div>
               </dl>
-              <div className="mt-6 border-t border-gray-900/5 px-6 py-6">
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Download receipt <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Invoice */}
-          <div className="-mx-4 px-4 py-8 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-16">
-            <h2 className="text-base font-semibold leading-6 text-gray-900">
-              Invoice
-            </h2>
-            <dl className="mt-6 grid grid-cols-1 text-sm leading-6 sm:grid-cols-2">
-              <div className="sm:pr-4">
-                <dt className="inline text-gray-500">Issued on</dt>{" "}
-                <dd className="inline text-gray-700">
-                  <time dateTime="2023-23-01">January 23, 2023</time>
-                </dd>
-              </div>
-              <div className="mt-2 sm:mt-0 sm:pl-4">
-                <dt className="inline text-gray-500">Due on</dt>{" "}
-                <dd className="inline text-gray-700">
-                  <time dateTime="2023-31-01">January 31, 2023</time>
-                </dd>
-              </div>
-              <div className="mt-6 border-t border-gray-900/5 pt-6 sm:pr-4">
-                <dt className="font-semibold text-gray-900">From</dt>
-                <dd className="mt-2 text-gray-500">
-                  <span className="font-medium text-gray-900">Acme, Inc.</span>
-                  <br />
-                  7363 Cynthia Pass
-                  <br />
-                  Toronto, ON N3Y 4H8
-                </dd>
-              </div>
-              <div className="mt-8 sm:mt-6 sm:border-t sm:border-gray-900/5 sm:pl-4 sm:pt-6">
-                <dt className="font-semibold text-gray-900">To</dt>
-                <dd className="mt-2 text-gray-500">
-                  <span className="font-medium text-gray-900">Tuple, Inc</span>
-                  <br />
-                  886 Walter Street
-                  <br />
-                  New York, NY 12345
-                </dd>
-              </div>
-            </dl>
-            <table className="mt-16 w-full whitespace-nowrap text-left text-sm leading-6">
-              <colgroup>
-                <col className="w-full" />
-                <col />
-                <col />
-                <col />
-              </colgroup>
-              <thead className="border-b border-gray-200 text-gray-900">
-                <tr>
-                  <th scope="col" className="px-0 py-3 font-semibold">
-                    Projects
-                  </th>
-                  <th
-                    scope="col"
-                    className="hidden py-3 pl-8 pr-0 text-right font-semibold sm:table-cell"
-                  >
-                    Hours
-                  </th>
-                  <th
-                    scope="col"
-                    className="hidden py-3 pl-8 pr-0 text-right font-semibold sm:table-cell"
-                  >
-                    Rate
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3 pl-8 pr-0 text-right font-semibold"
-                  >
-                    Price
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {invoice.items.map((item) => (
-                  <tr key={item.id} className="border-b border-gray-100">
-                    <td className="max-w-0 px-0 py-5 align-top">
-                      <div className="truncate font-medium text-gray-900">
-                        {item.title}
-                      </div>
-                      <div className="truncate text-gray-500">
-                        {item.description}
-                      </div>
-                    </td>
-                    <td className="hidden py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700 sm:table-cell">
-                      {item.hours}
-                    </td>
-                    <td className="hidden py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700 sm:table-cell">
-                      {item.rate}
-                    </td>
-                    <td className="py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700">
-                      {item.price}
+              <table className="mt-16 w-full whitespace-nowrap text-left text-sm leading-6">
+                <colgroup>
+                  <col className="w-full" />
+                  <col />
+                  <col />
+                  <col />
+                </colgroup>
+                <thead className="border-b border-gray-200 text-gray-900">
+                  <tr>
+                    <th scope="col" className="px-0 py-3 font-semibold">
+                      Product
+                    </th>
+                    <th
+                      scope="col"
+                      className="hidden py-3 pl-8 pr-0 text-right font-semibold sm:table-cell"
+                    >
+                      Batch
+                    </th>
+                    <th
+                      scope="col"
+                      className="hidden py-3 pl-8 pr-0 text-right font-semibold sm:table-cell"
+                    >
+                      Per Unit
+                    </th>
+                    <th
+                      scope="col"
+                      className="hidden py-3 pl-8 pr-0 text-right font-semibold sm:table-cell"
+                    >
+                      Quantity
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3 pl-8 pr-0 text-right font-semibold"
+                    >
+                      Price
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.order_items.map((item) => (
+                    <tr key={item.id} className="border-b border-gray-100">
+                      <td className="max-w-0 px-0 py-5 align-top">
+                        <div className="truncate font-medium text-gray-900">
+                          {item.product_name}
+                        </div>
+                      </td>
+                      <td className="hidden py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700 sm:table-cell">
+                        {item.batch}
+                      </td>
+                      <td className="py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700">
+                        {item.price}
+                      </td>
+                      <td className="hidden py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700 sm:table-cell">
+                        {item.quantity}
+                      </td>
+                      <td className="py-5 pl-8 pr-0 text-right align-top tabular-nums text-gray-700 font-bold">
+                        {item.price * item.quantity}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <th
+                      scope="row"
+                      className="px-0 pb-0 pt-6 font-normal text-gray-700 sm:hidden"
+                    >
+                      Subtotal
+                    </th>
+                    <th
+                      scope="row"
+                      colSpan={4}
+                      className="hidden px-0 pb-0 pt-6 text-right font-normal text-gray-700 sm:table-cell"
+                    >
+                      Subtotal
+                    </th>
+                    <td className="pb-0 pl-8 pr-0 pt-6 text-right tabular-nums text-gray-900">
+                      {invoice.subTotal}
                     </td>
                   </tr>
-                ))}
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th
-                    scope="row"
-                    className="px-0 pb-0 pt-6 font-normal text-gray-700 sm:hidden"
-                  >
-                    Subtotal
-                  </th>
-                  <th
-                    scope="row"
-                    colSpan={3}
-                    className="hidden px-0 pb-0 pt-6 text-right font-normal text-gray-700 sm:table-cell"
-                  >
-                    Subtotal
-                  </th>
-                  <td className="pb-0 pl-8 pr-0 pt-6 text-right tabular-nums text-gray-900">
-                    {invoice.subTotal}
-                  </td>
-                </tr>
-                <tr>
-                  <th
-                    scope="row"
-                    className="pt-4 font-normal text-gray-700 sm:hidden"
-                  >
-                    Tax
-                  </th>
-                  <th
-                    scope="row"
-                    colSpan={3}
-                    className="hidden pt-4 text-right font-normal text-gray-700 sm:table-cell"
-                  >
-                    Tax
-                  </th>
-                  <td className="pb-0 pl-8 pr-0 pt-4 text-right tabular-nums text-gray-900">
-                    {invoice.tax}
-                  </td>
-                </tr>
-                <tr>
-                  <th
-                    scope="row"
-                    className="pt-4 font-semibold text-gray-900 sm:hidden"
-                  >
-                    Total
-                  </th>
-                  <th
-                    scope="row"
-                    colSpan={3}
-                    className="hidden pt-4 text-right font-semibold text-gray-900 sm:table-cell"
-                  >
-                    Total
-                  </th>
-                  <td className="pb-0 pl-8 pr-0 pt-4 text-right font-semibold tabular-nums text-gray-900">
-                    {invoice.total}
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
+                  <tr>
+                    <th
+                      scope="row"
+                      className="pt-4 font-normal text-gray-700 sm:hidden"
+                    >
+                      Tax
+                    </th>
+                    <th
+                      scope="row"
+                      colSpan={4}
+                      className="hidden pt-4 text-right font-normal text-gray-700 sm:table-cell"
+                    >
+                      Tax
+                    </th>
+                    <td className="pb-0 pl-8 pr-0 pt-4 text-right tabular-nums text-gray-900">
+                      {invoice.tax}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th
+                      scope="row"
+                      className="pt-4 font-semibold text-gray-900 sm:hidden"
+                    >
+                      Total
+                    </th>
+                    <th
+                      scope="row"
+                      colSpan={4}
+                      className="hidden pt-4 text-right font-semibold text-gray-900 sm:table-cell"
+                    >
+                      Total
+                    </th>
+                    <td className="pb-0 pl-8 pr-0 pt-4 text-right font-semibold tabular-nums text-gray-900">
+                      {invoice.total}
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
